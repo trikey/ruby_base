733f64924f13c267161289d884e059177d1d32b7
Ivan Belitskii <belitskii@gmail.com>
2017-10-05 11:19:24 +0300

    сокращение кода

--- task_2/program_3.rb
+++ task_2/program_3.rb
@@ -1,9 +1,7 @@
 fibonachi_array = [0, 1]
 
-next_fib = fibonachi_array[-2] + fibonachi_array[-1]
-while next_fib < 100
+while (next_fib = fibonachi_array[-2] + fibonachi_array[-1]) < 100
   fibonachi_array << next_fib
-  next_fib = fibonachi_array[-2] + fibonachi_array[-1]
 end
 
 puts fibonachi_array.to_s
